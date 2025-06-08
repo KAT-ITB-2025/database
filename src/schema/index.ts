f123122e85e3a4e2825a5d79f5f31ed2a31bd382
ValentinoTriadi <13522164@std.stei.itb.ac.id>
2025-06-09 02:24:30 +0700

    style: reformat code

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,7 +1,3 @@
-import { db } from '~/migrate';
-
 export * from './user.schema';
 export * from './auth.schema';
 export * from './media.schema';
-
-export type Database = typeof db;
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "tsup"
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
-    splitting: true,
-    treeshake: true,
-    dts: true,
-    clean: true,
-    minify: true,
-    entry: ['src/schema.ts', 'scripts/adduser.ts'],
-    format: ['cjs', "esm"],
-})
+  splitting: true,
+  treeshake: true,
+  dts: true,
+  clean: true,
+  minify: true,
+  entry: ['src/index/schema.ts'],
+  format: ['cjs', 'esm'],
+});
